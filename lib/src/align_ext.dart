3036da044adea46fe31c5021a41914664e0ebb2c
Tyrant <>
2021-07-30 11:00:41 +0800

    增加对null-safety的支持

--- lib/src/align_ext.dart
+++ lib/src/align_ext.dart
@@ -2,30 +2,29 @@ import 'package:flutter/material.dart';
 
 extension AlignmentExt on Widget {
   // 对齐
-  Align align({Alignment by, Key key}) => Align(
+  Align align({AlignmentGeometry by = Alignment.center, Key? key}) => Align(
         alignment: by,
         key: key,
         child: this,
       );
 
   // 中心
-  Center centerized({Key key}) => Center(key: key, child: this);
+  Center centerized({Key? key}) => Center(key: key, child: this);
 }
 
 extension Sized on Widget {
-  Widget sized({Size to, Key key}) {
-    if (to == null) return this;
+  Widget sized({Size? to, Key? key}) {
     return SizedBox(
       key: key,
-      width: to.width,
-      height: to.height,
+      width: to?.width,
+      height: to?.height,
       child: this,
     );
   }
 }
 
 extension Containered on Widget {
-  Widget inContainer({Key key, Decoration decoration, Color color}) =>
+  Widget inContainer({Key? key, Decoration? decoration, Color? color}) =>
       Container(
         key: key,
         decoration: decoration,
@@ -35,19 +34,19 @@ extension Containered on Widget {
 }
 
 extension PositionExt on Widget {
-  Padding paddingBy({EdgeInsetsGeometry padding, Key key}) => Padding(
+  Padding paddingBy({required EdgeInsetsGeometry padding, Key? key}) => Padding(
         key: key,
         padding: padding,
         child: this,
       );
 
   Positioned positionedBy({
-    double left,
-    double top,
-    double right,
-    double bottom,
-    double width,
-    double height,
+    double? left,
+    double? top,
+    double? right,
+    double? bottom,
+    double? width,
+    double? height,
   }) =>
       Positioned(
           left: left,
