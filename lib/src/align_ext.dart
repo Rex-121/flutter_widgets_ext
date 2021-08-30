2517274459703203212da5fe2a44eb2f28733385
Tyrant <>
2021-08-30 15:06:21 +0800

    修改部分方法

--- lib/src/align_ext.dart
+++ lib/src/align_ext.dart
@@ -25,21 +25,31 @@ extension Sized on Widget {
 }
 
 extension Containered on Widget {
-  Widget inContainer({Key key, Decoration decoration, Color color}) =>
-      Container(
-        key: key,
-        decoration: decoration,
-        color: color,
-        child: this,
-      );
+  Widget inContainer(
+      {Key key,
+      Decoration decoration,
+      Color color,
+      double weight,
+      double height}) {
+    return Container(
+      key: key,
+      width: weight,
+      height: height,
+      decoration: decoration,
+      color: color,
+      child: this,
+    );
+  }
 }
 
 extension PositionExt on Widget {
-  Padding paddingBy({EdgeInsetsGeometry padding, Key key}) => Padding(
-        key: key,
-        padding: padding,
-        child: this,
-      );
+  Padding paddingBy({EdgeInsetsGeometry padding, Key key}) {
+    return Padding(
+      key: key,
+      padding: padding,
+      child: this,
+    );
+  }
 
   Positioned positionedBy({
     double left,
@@ -48,13 +58,20 @@ extension PositionExt on Widget {
     double bottom,
     double width,
     double height,
-  }) =>
-      Positioned(
-          left: left,
-          top: top,
-          right: right,
-          bottom: bottom,
-          width: width,
-          height: height,
-          child: this);
+  }) {
+    return Positioned(
+        left: left,
+        top: top,
+        right: right,
+        bottom: bottom,
+        width: width,
+        height: height,
+        child: this);
+  }
+}
+
+extension FlexExt on Widget {
+  Expanded expanded({@required int byFlex, Key key}) {
+    return Expanded(key: key, flex: byFlex, child: this);
+  }
 }
